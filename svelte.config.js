0387bb3559b3b0e40cc555e7c7b603a6a57efddb
James Xylight <james@xylight.dev>
2025-05-24 16:06:22 -0700

    misc: switch to cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import { mdsvex } from 'mdsvex'
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import staticAdapter from '@sveltejs/adapter-static'
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: staticAdapter() },
+	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 }
 
