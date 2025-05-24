92dbd56557fb8024239a6c5f471f4833ef47ba7c
James Xylight <james@xylight.dev>
2025-05-24 16:12:03 -0700

    fix: added meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,13 @@
 	let { children } = $props()
 </script>
 
+<svelte:head>
+	<title>xylight.dev</title>
+	<meta name="keywords" content="developer, portfolio, xylight" />
+	<meta name="author" content="Xylight" />
+	<meta name="description" content="Xylight's portfolio" />
+</svelte:head>
+
 <div class="min-h-screen w-full flex flex-col">
 	{@render children()}
 	<footer class="mt-auto flex flex-row px-8 w-full text-zinc-400 font-medium text-sm py-8 gap-4">
